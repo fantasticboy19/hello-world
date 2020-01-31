96acbd06ae60b6b54229280dc919a8855974fe81
fantasticboy19 <2444093230@qq.com>
2020-01-31 20:20:51 +0800

    ‘git commit -a'

--- print.py
+++ print.py
@@ -4,3 +4,13 @@ for i in os.listdir(path):
     print(i)
 print('os.path')
 os.path.append('c:\user\desktop\hello.exe')
+
+class baseNet(nn.Module){
+        def __init__(self):
+            self.con = nn.conv2d(20,3,10)
+            self.relu = nn.Relu(10,5)
+
+        def forwards(self,x):
+            output = self.con(x)
+            return self.relu(output)
+            }
